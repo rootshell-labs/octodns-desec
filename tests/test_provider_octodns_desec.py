a0e87e9eaeab44633907588080e6939ffe44e62a
TilCreator <tilcreator@tc-j.de>
2024-07-17 21:43:43 +0200

    Init
    Co-authored-by: raven/sky <raven@mwsq.de>

---
+++ tests/test_provider_octodns_desec.py
@@ -0,0 +1,14 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_desec import DesecProvider
+
+
+class TestDesecProvider(TestCase):
+    # TODO: test provider
+    def test_nothing(self):
+        self.assertTrue(True)
+        DesecProvider
